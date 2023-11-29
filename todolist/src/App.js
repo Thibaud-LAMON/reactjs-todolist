8738582174a83ef57fb0e5df91f834a0d885e5cd
Thibaud-LAMON <thib.lamon@gmail.com>
2023-11-29 16:41:13 +0100

    5.	Utiliser Context API : créer un objet context

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -5,57 +5,7 @@ import "./App.css";
 import { v4 as uuid } from "uuid";
 //on a installé uuid qui génère une suite de chiffres et lettres pour créer un id unique
 
-//on définit un état initial
-const initialState = {
-  items: [
-    { id: 1, content: "pay bills", done: true },
-    { id: 2, content: "learn React", done: false },
-  ], //pour les résultats filtrés
-  all: [
-    { id: 1, content: "pay bills", done: true },
-    { id: 2, content: "learn React", done: false },
-  ], //pour la liste complête
-  input: null,
-};
-
-//on définit une fonction Reducer. 2 params retournés : state et action(suite à l'action de distribuer)
-//on y met toute la logique pour s'occuper de la gestion d'état de façon isolée et séparée.
-function reducer(state, action) {
-  //switch pour faire du contrôle de flux
-  switch (action.type) {
-    case "submit": //dans le cas de l'action submit
-      return {
-        ...state, //on copie l'état
-        items: [...state.items, action.payload.item], //puis on màj items, pour cela on copie leur état, puis on rajoute un élément transmis en tant que payload
-      };
-    case "change": //dans le cas de l'action change
-      return {
-        ...state,
-        input: action.payload.value, //input correspond à value = e.target.value
-      };
-    case "check": //dans le cas de l'action check
-      const updated = state.items.map((item) =>
-        item.id === action.payload.id
-          ? { ...item, done: action.payload.bool }
-          : item
-      );
-      //Pour chaque élément on retourne un tableau avec les éléments transformés.
-      //On compare l'id de l'item avec l'id transmit en paramètre, si oui la valeur booléenne est mise à jour, sinon on renvoi l'item
-      return {
-        ...state,
-        items: updated, //remplace setItems dans handleOnCheck
-        all: updated, //remplace setAll dans handleOnCheck
-      };
-    case "select": //dans le cas de l'action select
-      const filtered = state.items.filter((item) => !!item.done); //renvoie les item qui ont le statut done
-      return {
-        ...state,
-        items: action.payload.option === "Completed" ? filtered : state.all, //remplace setItems dans handleOnSelect
-      };
-    default:
-      throw new Error();
-  }
-}
+//On déplace le reducer et l'initial state dans contexte
 
 const Container = ({ children, title }) => {
   return (
@@ -156,9 +106,7 @@ function List({ items, onCheck }) {
 }
 
 function App() {
-  //useReducer renvoie des valeurs sous forme de tableau
-  //le tableau retourne l'état et un dispatch qui distribue les actions
-  const [state, dispatch] = useReducer(reducer, initialState); //2 args: reducer = fonction qui fait les calculs des nouveaux states, initialState = valeur initalle
+  //on déplace aussi le useReducer
   const ref = useRef(); //on créer une ref pour accéder au noeud du DOM
 
   //Fonctions de changement d'état
---
+++ todolist/src/context.js
@@ -0,0 +1,70 @@
+//On commence par créer context.js
+import { createContext, useReducer } from "react"; //on importe createContext
+
+const Context = createContext(); //on le récupère
+const { Provider, Consumer } = Context; //Context donne accès à Provider et Consumer
+//Provider fournit les informations (valeurs globales) à tous les composants descendant du Provider
+
+//on définit un état initial
+const initialState = {
+  items: [
+    { id: 1, content: "pay bills", done: true },
+    { id: 2, content: "learn React", done: false },
+  ], //pour les résultats filtrés
+  all: [
+    { id: 1, content: "pay bills", done: true },
+    { id: 2, content: "learn React", done: false },
+  ], //pour la liste complête
+  input: null,
+};
+
+//on définit une fonction Reducer. 2 params retournés : state et action(suite à l'action de distribuer)
+//on y met toute la logique pour s'occuper de la gestion d'état de façon isolée et séparée.
+function reducer(state, action) {
+  //switch pour faire du contrôle de flux
+  switch (action.type) {
+    case "submit": //dans le cas de l'action submit
+      return {
+        ...state, //on copie l'état
+        items: [...state.items, action.payload.item], //puis on màj items, pour cela on copie leur état, puis on rajoute un élément transmis en tant que payload
+      };
+    case "change": //dans le cas de l'action change
+      return {
+        ...state,
+        input: action.payload.value, //input correspond à value = e.target.value
+      };
+    case "check": //dans le cas de l'action check
+      const updated = state.items.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, done: action.payload.bool }
+          : item
+      );
+      //Pour chaque élément on retourne un tableau avec les éléments transformés.
+      //On compare l'id de l'item avec l'id transmit en paramètre, si oui la valeur booléenne est mise à jour, sinon on renvoi l'item
+      return {
+        ...state,
+        items: updated, //remplace setItems dans handleOnCheck
+        all: updated, //remplace setAll dans handleOnCheck
+      };
+    case "select": //dans le cas de l'action select
+      const filtered = state.items.filter((item) => !!item.done); //renvoie les item qui ont le statut done
+      return {
+        ...state,
+        items: action.payload.option === "Completed" ? filtered : state.all, //remplace setItems dans handleOnSelect
+      };
+    default:
+      throw new Error();
+  }
+}
+
+//Nouveau composant
+//Il va transmettre les éléments descendants, qui seront enveloppés par le provider
+const AppProvider = ({ children }) => {
+  //useReducer renvoie des valeurs sous forme de tableau
+  //le tableau retourne l'état et un dispatch qui distribue les actions
+  const [state, dispatch] = useReducer(reducer, initialState); //2 args: reducer = fonction qui fait les calculs des nouveaux states, initialState = valeur initalle
+  //le useReducer est également déplacé
+  //il retourne sous forme de tableau le state et le dispatch et on le transmet comme value au Provider
+  return <Provider value={{}}>{children}</Provider>; //Tout composant encapsulé par Provider est un enfant et aura accès aux valeurs globales
+  //unique prop -> value
+};
